53191e7e85af0ee1e7e56835a03669f6a4d21524
Jakub Jarosiński <dzejkop@DESKTOP-LV2Q3C1.localdomain>
2022-05-29 22:55:27 +0200

    Should be working (perhabs)

--- main.py
+++ main.py
@@ -1,111 +1,94 @@
-# Liczba akcji n
-# Jedna akcja to wypozycz, oddaj
-# Można wypozczyć tylko jeden egzemplarz danej ksiazki 
-# Można maksymalnie wypozczyć 3 egzemplaże książek 
-'''
-12
-(" dodaj ", "Pan Tadeusz ", " Adam Mickiewicz ", 2000)
-(" dodaj ", "Quo Vadis ", " Henryk Sienkiewicz ", 2010)
-(" dodaj ", " Chatka Puchatka ", " Alan Alexander Milne ", 1998)
-(" dodaj ", "Pan Tadeusz ", " Adam Mickiewicz ", 2000)
-(" dodaj ", " Chatka Puchatka ", " Alan Alexander Milne ", 2014)
-(" wypozycz ", " Bartek Perkowski ", "Pan Tadeusz ")
-(" wypozycz ", " Bartek Perkowski ", "Pan Tadeusz ")
-(" wypozycz ", " Jacek Malyszko ", "Quo Vadis ")
-(" wypozycz ", " Bartek Perkowski ", "Quo Vadis ")
-(" oddaj ", " Jacek Malyszko ", "Quo Vadis ")
-(" wypozycz ", " Bartek Perkowski ", "Quo Vadis ")
-(" oddaj ", " Jacek Malyszko ", "Quo Vadis ")
+class Biblioteka():
 
-'''
-
-
-class Biblioteka(object):
-
-        def __init__(self):
-            self.books = []
-            self.copies  = []
-            self.readers = []
-            self.titles = []
-            self.wynik=[]
+    
+    def __init__(self):
+        self.books = []
+        self.titles=[]
+        self.readers= {}
+        self.wyniki=[]
+        self.copies=[]
 
 
 
-        def dodajKsiazke(self, ksiazka):
-            self.books.append(ksiazka)
 
-
-        #Stworz pojedynczy egzemplarz i dodaj do listy egzemplarzy
-        def addCopy(self, egzemplarz):
-            el1 = list(egzemplarz)
-            # Jeśli egzemplarz znajduje sie w liscie ksiazek
-            if el1[1:3] in self.books:
-                self.copies.append(el1[1:3])
-                self.titles.append(el1[1])
-                self.wynik.append("True")
-
-            else:
-            # Jeśli egzemplarz nie znajduje się w liście ksiazek
-                self.dodajKsiazke(el1[1:3])
-                self.titles.append(el1[1])
-                self.copies.append(el1[1:3])
-                self.wynik.append('True')
+    def addBook(self, book):
+        self.books.append(book)
 
 
 
-        def reader(self, name, title):
-            if name in self.readers:
-                if len(self.readers[name]) < 3 and title not in self.readers[name]:
-                    self.readers[name].append(title)
-                    self.titles.remove(title)
-                    self.wynik.append('True')
-                else:
-                    self.wynik.append('False')
-            else:
-                self.readers[name]=[]
+    def addCopy(self,copy):
+        copy1 = list(copy)
+        if copy1[1:3] in self.books:
+            self.copies.append(copy1[1:3])
+            self.titles.append(copy1[1])
+            self.wyniki.append('True')
+        else:
+            library.addBook(copy1[1:3])
+            self.copies.append(copy1[1:3])
+            self.titles.append(copy1[1])
+            self.wyniki.append('True')
+
+
+
+
+
+
+    def counting(self):
+        final_list=[]
+        sort_list=[]
+        for book in self.books:
+            copies_counted = self.copies.count(book)
+            book1 = [book[0], book[1], copies_counted]
+            final_list.append(book1)
+        sort_list = sorted(final_list, key=lambda x: x[0])
+        for el in sort_list:
+            el1 = (el[0], el[1], el[2])
+            print(el1)
+
+
+    def reader(self, name, title):
+        if name in self.readers:
+            if len(self.readers[name]) < 3 and title not in self.readers[name]:
                 self.readers[name].append(title)
                 self.titles.remove(title)
-                self.wynik.append('True')
-
-
-
-
-        def borrow(self):
-            if title in self.titles:
-                self.reader(name,title)
+                self.wyniki.append('True')
             else:
-                self.wynik.append('False')
+                self.wyniki.append('False')
+        else:
+            self.readers[name]=[]
+            self.readers[name].append(title)
+            self.titles.remove(title)
+            self.wyniki.append('True')
 
 
 
-        def returnBack(self,name,title):
-            if name in self.readers:
-                if title in self.readers[name]:
-                    self.readers[name].remove(title)
-                    self.titles.append(title)
-                    self.wynik.append('True')
-                else:
-                    self.wynik.append('False')
+    def borrow(self,name,title):
+        if title in self.titles:
+            self.reader(name,title)
+        else:
+            self.wyniki.append('False')
+
+
+
+
+
+    def oddaj(self,name,title):
+        if name in self.readers:
+            if title in self.readers[name]:
+                self.readers[name].remove(title)
+                self.titles.append(title)
+                self.wyniki.append('True')
             else:
-                self.wynik.append('False')
+                self.wyniki.append('False')
 
 
-        def calculation(self):
-            final_list = []
-            sort_list  = []
-            for ksiazka in self.books:
-                a = self.copies .count(ksiazka)
-                ksiazka1 = [ksiazka[0], ksiazka[1],a]
-                final_list.append(ksiazka1)
-                sort_list = sorted(final_list, key=lambda x: x[0])
-                for element in sort_list:
-                    element1 = (element[0], element[1], element[2])
-                    print(element1)
 
+        else:
+            self.wyniki.append('False')
 
-        def pokaWynik(self):
-            for w in self.wynik:
-             print(w)
+    def podajWynik(self):
+        for wynik in self.wyniki:
+            print(wynik)
 
 
 
@@ -113,15 +96,17 @@ class Biblioteka(object):
 
 biblio = Biblioteka()
 
+
+
 n = int(input())
 
 for i in range(0, n):
-    wejscie = eval(input())
-    if wejscie[0] == "dodaj":
-        biblio.addCopy(wejscie)
-    elif wejscie[0] == "borrow":
-        biblio.borrow(wejscie[1],wejscie[2])
-    elif wejscie[0] == "oddaj":
-        biblio.returnBack(wejscie[1],wejscie[2])
+    x = eval(input())
+    if x[0] == "dodaj":
+        library.addCopy(x)
+    elif x[0] == "wyporzycz":
+        library.borrow(x[1],x[2])
+    elif x[0] == "oddaj":
+        library.oddaj(x[1],x[2])
 
-biblio.pokaWynik()
+biblio.podajWynik()
